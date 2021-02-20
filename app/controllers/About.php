47ede7bb8f9373a45bc5c6592718a98ae2027449
hilmialmuhtadeb <hilmialmuhtadeb@gmail.com>
2021-02-20 13:11:49 +0700

    belajar materi view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -5,9 +5,17 @@ class About extends Controller {
     $data['nama'] = $nama;
     $data['pekerjaan'] = $pekerjaan;
     $data['umur'] = $umur;
+    $data['judul'] = 'About';
+
+    $this->view('templates/header', $data);
     $this->view('about/index', $data);
+    $this->view('templates/footer');
   }
   public function page() {
+    $data['judul'] = 'page';
+
+    $this->view('templates/header', $data);
     $this->view('about/page');
+    $this->view('templates/footer');
   }
 }
